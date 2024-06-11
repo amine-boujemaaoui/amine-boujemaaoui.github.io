5d7d9b862db48f6b63b21869dc60864cb745074e
Amine Boujemaaoui <92468393+amine-boujemaaoui@users.noreply.github.com>
2024-06-11 19:16:25 +0200

    dropdown

--- portfolio/script.js
+++ portfolio/script.js
@@ -1,33 +1,52 @@
 function switchLanguage(lang) {
-    const url = window.location.href;
-    if (lang === 'en') {
-        if (url.includes('index.html')) {
-            window.location.href = 'index_en.html';
-        } else if (url.includes('resume.html')) {
-            window.location.href = 'resume_en.html';
-        } else if (url.includes('about.html')) {
-            window.location.href = 'about_en.html';
-        }
-    } else if (lang === 'fr') {
-        if (url.includes('index_en.html')) {
-            window.location.href = 'index.html';
-        } else if (url.includes('resume_en.html')) {
-            window.location.href = 'resume.html';
-        } else if (url.includes('about_en.html')) {
-            window.location.href = 'about.html';
-        }
-    } else if (lang === 'it') {
-        if (url.includes('index.html') || url.includes('index_en.html')) {
-            window.location.href = 'index_it.html';
-        } else if (url.includes('resume.html') || url.includes('resume_en.html')) {
-            window.location.href = 'resume_it.html';
-        } else if (url.includes('about.html') || url.includes('about_en.html')) {
-            window.location.href = 'about_it.html';
-        }
+    const currentUrl = new URL(window.location.href);
+    const currentPath = currentUrl.pathname;
+
+    let newPath;
+    if (currentPath.includes('index')) {
+        newPath = lang === 'fr' ? 'index.html' : `index_${lang}.html`;
+    } else if (currentPath.includes('resume')) {
+        newPath = lang === 'fr' ? 'resume.html' : `resume_${lang}.html`;
+    } else if (currentPath.includes('about')) {
+        newPath = lang === 'fr' ? 'about.html' : `about_${lang}.html`;
+    } else {
+        newPath = lang === 'fr' ? 'index.html' : `index_${lang}.html`; // Default to index page if no match
+    }
+
+    window.location.href = newPath;
+}
+
+function setInitialLanguage() {
+    const userLang = navigator.language || navigator.userLanguage;
+    const langCode = userLang.split('-')[0];
+    const supportedLangs = ['en', 'fr', 'it'];
+    const defaultLang = 'fr';
+
+    let selectedLang = supportedLangs.includes(langCode) ? langCode : defaultLang;
+
+    const currentUrl = new URL(window.location.href);
+    const currentPath = currentUrl.pathname;
+
+    const currentLang = supportedLangs.find(lang => lang === 'fr' ? !currentPath.includes('_en') && !currentPath.includes('_it') : currentPath.includes(`_${lang}`));
+    if (!currentLang) {
+        switchLanguage(selectedLang);
+    } else {
+        updateFlag(currentLang);
     }
 }
 
+function updateFlag(lang) {
+    const flagSrc = {
+        'en': 'en.svg',
+        'fr': 'fr.svg',
+        'it': 'it.svg'
+    };
+    document.getElementById('selected-lang-btn').innerHTML = `<img src="${flagSrc[lang]}" alt="${lang}" class="flag">`;
+}
+
 document.addEventListener("DOMContentLoaded", function() {
+    setInitialLanguage();
+
     if (window.location.pathname.includes('resume')) {
         const lang = window.location.pathname.includes('_en') ? 'en' : (window.location.pathname.includes('_it') ? 'it' : 'fr');
         const xmlFile = lang === 'en' ? 'cv_en.xml' : (lang === 'it' ? 'cv_it.xml' : 'cv.xml');
